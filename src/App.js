9afe9c5dcdb05d00d0fb824c04e1318ef214d911
siddhantprateek <siddhantprateek@gmail.com>
2021-08-29 17:31:59 +0530

    monster Search Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.components';
+import { SearchBox } from './components/search-box/search-box.components';
+class App extends Component{
+
+  constructor(){
+    super();
+
+    this.state = {
+        users: [],
+        searchField: ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({users: users}))
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+
+  render(){
+    const {users, searchField} = this.state;
+    const filteredUsers = users.filter( user => 
+      user.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
+    return(
+      <div className="App">
+        <h1>Our Customers</h1>
+        <SearchBox 
+          placeholder='Search User'
+          handleChange={this.handleChange}/>
+        <CardList users={filteredUsers}/>
+      </div>
+    );
+  }
 }
 
 export default App;
